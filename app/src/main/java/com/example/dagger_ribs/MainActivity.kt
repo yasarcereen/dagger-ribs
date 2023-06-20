60697b8d40ee3e80beeb7b3bc2c599f302f1feda
Ceren Yaşar <yasarcereen@gmail.com>
2023-06-20 13:30:42 +0300

    login view update

--- app/src/main/java/com/example/dagger_ribs/MainActivity.kt
+++ app/src/main/java/com/example/dagger_ribs/MainActivity.kt
@@ -3,36 +3,36 @@ package com.example.dagger_ribs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dagger_ribs.ui.theme.DaggerribsTheme
+import com.example.dagger_ribs.units.login.LoginView
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val loginView = LoginView()
+
         setContent {
             DaggerribsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                loginView.Content(modifier = Modifier)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
--- app/src/main/java/com/example/dagger_ribs/units/login/LoginView.kt
+++ app/src/main/java/com/example/dagger_ribs/units/login/LoginView.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.example.dagger_ribs.R
 import com.example.dagger_ribs.utils.compose.rib.Compose
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
@@ -23,23 +27,25 @@ class LoginView : Compose {
     private val emailText = MutableStateFlow("")
     private val passwordText = MutableStateFlow("")
 
+    private val buttonClick = MutableSharedFlow<Unit>()
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content(modifier: Modifier) {
         val coroutineScope = rememberCoroutineScope()
 
-        Column {
+        Column(modifier = Modifier
+            .padding(start = 24.dp, end = 24.dp, top = 10.dp, bottom = 10.dp)
+            .fillMaxWidth()) {
             Spacer(modifier = Modifier.height(10.dp))
 
-            Text(modifier = Modifier
-                .padding(end = 56.dp, start = 24.dp)
-                .fillMaxWidth(),
+            Text(
                 text = stringResource(id = R.string.login_dialog_title)
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            TextField(value = "email",
+            OutlinedTextField(value = "email",
                 onValueChange = {
                     coroutineScope.launch {
                         emailText.emit(it)
@@ -47,13 +53,33 @@ class LoginView : Compose {
                 }
             )
 
-            TextField(value = "password",
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(value = "password",
                 onValueChange = {
                     coroutineScope.launch {
                         passwordText.emit(it)
                     }
                 }
             )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            ContinueButton()
+        }
+    }
+
+    @Composable
+    private fun ContinueButton() {
+        val coroutineScope = rememberCoroutineScope()
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    buttonClick.emit(Unit)
+                }
+            }) {
+            Text(text = "Continue")
         }
     }
 
